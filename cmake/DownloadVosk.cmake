cb0d405e97bd76c1e8e6bbc9b0770f11b3b75b96
RRostislaW <megarosplay@gmail.com>
2026-04-09 01:19:05 +0300

    auto vosk install

---
+++ cmake/DownloadVosk.cmake
@@ -0,0 +1,17 @@
+if(NOT EXISTS ${CMAKE_SOURCE_DIR}/core/include/vosk-include/libvosk.so OR NOT EXISTS ${CMAKE_SOURCE_DIR}/core/include/vosk-include/vosk_api.h)
+file(DOWNLOAD https://github.com/alphacep/vosk-api/releases/download/v0.3.45/vosk-linux-aarch64-0.3.45.zip ${CMAKE_SOURCE_DIR}/core/include/vosk-linux-aarch64-0.3.45.zip STATUS DownloadVoskStatus SHOW_PROGRESS)
+
+list(GET DownloadVoskStatus 0 DownloadVoskStatus_code)
+list(GET DownloadVoskStatus 1 DownloadVoskStatus_message)
+
+if(DownloadVoskStatus_code EQUAL 0)
+    message(STATUS "Download successful!")
+else()
+    message(FATAL_ERROR "Download failed: ${DownloadVoskStatus_message}")
+endif()
+
+file(ARCHIVE_EXTRACT INPUT ${CMAKE_SOURCE_DIR}/core/include/vosk-linux-aarch64-0.3.45.zip DESTINATION ${CMAKE_SOURCE_DIR}/core/include/)
+file(RENAME ${CMAKE_SOURCE_DIR}/core/include/vosk-linux-aarch64-0.3.45 ${CMAKE_SOURCE_DIR}/core/include/vosk-include)
+file(REMOVE ${CMAKE_SOURCE_DIR}/core/include/vosk-linux-aarch64-0.3.45.zip)
+
+endif()
