5250076acc07f9ed5fd06f2ba05db7d53ec73c69
RRostislaW <megarosplay@gmail.com>
2026-04-10 14:39:50 +0300

    change module DownloadVoskModel.cmake

--- cmake/DownloadVoskModel.cmake
+++ cmake/DownloadVoskModel.cmake
@@ -2,8 +2,8 @@ if(NOT EXISTS ${CMAKE_BINARY_DIR}/models/vosk-model-small-ru-0.22/)
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/models/)
 file(DOWNLOAD https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip ${CMAKE_BINARY_DIR}/models/vosk-model-small-ru-0.22.zip STATUS DownloadVoskModelStatus SHOW_PROGRESS)
 
-list(get DownloadVoskModelStatus 0 DownloadVoskModelStatus_code)
-list(get DownloadVoskModelStatus 1 DownloadVoskModelStatus_message)
+list(GET DownloadVoskModelStatus 0 DownloadVoskModelStatus_code)
+list(GET DownloadVoskModelStatus 1 DownloadVoskModelStatus_message)
 
 if(DownloadVoskModelStatus_code EQUAL 0)
     message("Download succesful")
