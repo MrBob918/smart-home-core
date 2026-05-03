68fadf087c79f11780fc82e1a703ceacb9b7bd55
MrBob918 <megarosplay@gmail.com>
2026-05-03 11:46:36 +0300

    refactor architecture

--- stt-server/src/stt-main.cpp
+++
@@ -1,9 +0,0 @@
-#include "stt.h"
-#include "proto/stt.pb.h"
-#include "proto/stt.grpc.pb.h"
-#include <iostream>
-int main() {
-    stt sttClass("./models/vosk-model-small-ru-0.22", 44100);
-    sttClass.transcribeAudio("./Voice_test.wav");
-    std::cout << sttClass.getFinalResult() << std::endl;
-}
